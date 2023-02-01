055266e699c381e3ee87a9832fda3aa50287afbb
shivam batho <shivam.b@fortunebusinessinsights.com>
2023-02-01 15:29:25 +0530

    Update index.php
    modified this files

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
-    echo "Hello WOrld";
-	
-	xghsdgh
-?>
+ $data = [1,2,3,4,5,6,7,8,9,10];
+ ?>
+<ol>
+ <?php
+ foreach($data as $val){
+    ?>
+    <li> <?php  echo $val ;  ?></li><br/>      
+    <?php
+ }
 
+?>
+</ol>
